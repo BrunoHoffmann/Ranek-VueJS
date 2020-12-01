dcee83677db685650d20d48ffdd2a785da6c6391
Bruno Hoffmann <brunooficial547@hotmail.com>
2020-12-01 13:29:11 -0300

    create form user register

--- client/src/components/LoginCriar.vue
+++ client/src/components/LoginCriar.vue
@@ -4,7 +4,7 @@
     <transition mode="out-in">
       <button v-if="!criar" class="btn" @click="criar = true">Criar Conta</button>
       <UsuarioForm v-else>
-        <button class="btn btn-form">Criar Usuário</button>
+        <button class="btn btn-form" @click.prevent="criarUsuario">Criar Usuário</button>
       </UsuarioForm>
     </transition>
   </section>
@@ -22,6 +22,17 @@ export default {
     return {
       criar: false,
     };
+  },
+  methods: {
+    async criarUsuario() {
+      try {
+        await this.$store.dispatch('criarUsuario', this.$store.state.usuario);
+        await this.$store.dispatch('getUsuario', this.$store.state.usuario.email);
+        this.$router.push({name: 'usuario'});
+      } catch(error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -31,10 +31,14 @@ export default new Vuex.Store({
   },
   actions: {
     getUsuario(context, payload) {
-      api.get(`/usuario/${payload}`).then(response => {
+      return api.get(`/usuario/${payload}`).then(response => {
         context.commit('UPDATE_USUARIO', response.data);
         context.commit('UPDATE_LOGIN', true);
       });
     },
+    criarUsuario(context, payload) {
+      context.commit('UPDATE_USUARIO', { id: payload.email });
+      return api.post('/usuario', payload);
+    }
   },
 })
